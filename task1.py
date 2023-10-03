c6fb3d521d123453412a91cf03f349e3ef83906a
ocalde224 <oliverc432@deltalearns.ca>
2023-10-03 12:36:37 -0700

    done

--- task1.py
+++ task1.py
@@ -19,3 +19,17 @@ The number is 100
 Enter a number: 102
 The number is larger than 100
 """
+q1 = "what is the number:"
+print(input(q1))
+thenumberislargerthan100 = True
+thenunumberis100 = False
+thenumberissmallerthan100 = False
+
+if thenumberislargerthan100 :
+    print("the number is larger than 100")
+
+if thenunumberis100:
+    print("the number is 100")
+
+if thenumberissmallerthan100:
+    print("the number is smaller than 100")
